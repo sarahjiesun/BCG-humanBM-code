145431a19830f2b84037286e4488d639da9bb27b
Sarah Sun <42935401+sarahjiesun@users.noreply.github.com>
2024-07-02 08:25:26 -0500

    Update and rename 1_UMAP_label_clusters.R to 01_UMAP_MPP_score.R

--- scRNA_analyses/2_main_analyses/HSC_subcluster_analyses/01_UMAP_MPP_score.R
+++ scRNA_analyses/2_main_analyses/HSC_subcluster_analyses/01_UMAP_MPP_score.R
@@ -4,8 +4,6 @@
 #hdf5/1.12.0 loaded 
 #cmake loaded
 
-.libPaths("/project/lbarreiro/USERS/sarah/Rlibs_new")
-
 library(ggplot2)
 library(cowplot)
 library(reticulate)
@@ -21,32 +19,28 @@ library(data.table)
 library(stringr)
 library(ggpubr)
 
+# In this script we make a UMAP of the HSC subclusters and plot various variables for QC
+# We also calculate an MPP score for each HSC subcluster based on the average expression of "THY1", "PTPRC", and "ITGA6"
 
 # setup -------------------------------------------------------------------
-
 options(future.globals.maxSize = 1000 * 1024^2)
-setwd("/project/lbarreiro/USERS/sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/Rprojects/projects_version2_Rv4.1/Analysis_Raul")
-
-IN_DIR <- "HSC_subcluster_analysis/0_SCT_cluster/"
-OUT_DIR <- "HSC_subcluster_analysis/1_UMAP_label_clusters/"
+setwd("/scRNA_analyses/2_main_analyses/HSC_subcluster_analyses")
+IN_DIR <- "0_SCT_cluster/"
+OUT_DIR <- "1_UMAP_label_clusters/"
 dir.create(OUT_DIR)
 
 
-# Read in merged_scaled_integrated_clustered object -----------------------
-
+# Read in merged_scaled_integrated_clustered HSC object -----------------------
 allCells_integrated <- readRDS(file = paste0(IN_DIR, "HSC_obj_SCT_integrated_withClusters.rds"))
 
 
-
 # print UMAP --------------------------------------------------------------
-
 pdf(paste0(OUT_DIR,"UMAP.pdf"), width=7, height=5)
 print(DimPlot(allCells_integrated, reduction = "umap", label = TRUE) + NoLegend())
 dev.off()
 
 
 # Plot categorical covariates ---------------------------------------------
-
 covariates <- c("vaccination", "timepoint", "batchID")
 for(i in 1:length(covariates))
 {
@@ -57,7 +51,6 @@ for(i in 1:length(covariates))
 }
 
 # Plot numeric covariates -------------------------------------------------
-
 covariates <- c("nCount_RNA", "nFeature_RNA", "percent.mt")
 for(i in 1:length(covariates)){
   covar <- covariates[i]
@@ -65,7 +58,6 @@ for(i in 1:length(covariates)){
   print(FeaturePlot(allCells_integrated, reduction = "umap", features = covar))
   dev.off()
 }
-
 
 pdf(paste0(OUT_DIR,"UMAP_split_CTL_BCG.pdf"), width = 10, height = 4)
 DimPlot(allCells_integrated, reduction = "umap", split.by = "vaccination")
@@ -79,8 +71,8 @@ png(paste0(OUT_DIR,"single_cell_stats.png"), width = 2160, height = 720)
 VlnPlot(allCells_integrated, features=c("nCount_RNA","nFeature_RNA","percent.mt"))
 dev.off()
 
-# Label cells with UNIQUE sample label combining donor+timepoint+vaccination ------------------------------------
 
+# Label cells with UNIQUE sample label combining donor+timepoint+vaccination ------------------------------------
 UNIQUE_ID <- vector(length=length(row.names(allCells_integrated@meta.data)))
 
 for (i in 1:length(row.names(allCells_integrated@meta.data)))
@@ -89,14 +81,10 @@ for (i in 1:length(row.names(allCells_integrated@meta.data)))
 }
 
 allCells_integrated$UNIQUE_ID <- UNIQUE_ID
-
 saveRDS(allCells_integrated, file=paste0(OUT_DIR,"HSC_final_UNIQUE_IDS.rds"))
 
 
-
-
 # Find markers for every cluster ------------------------------------------
-
 HSC.markers <- FindAllMarkers(allCells_integrated, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 
 HSC_markers_data <- data.frame(HSC.markers %>%
@@ -110,8 +98,12 @@ FeaturePlot(allCells_integrated, features = c("THY1"), reduction = "umap", cols=
 FeaturePlot(allCells_integrated, features = c("PTPRC"), reduction = "umap", cols=c('lightgrey', '#BA68C8')) #CD45RA
 FeaturePlot(allCells_integrated, features = c("ITGA6"), reduction = "umap", cols=c('lightgrey', '#BA68C8')) #CD49f
 
-# look for MPP expression signatures --------------------------------------
 
+######################
+###   MPP score    ###
+######################
+
+# look for MPP expression signatures --------------------------------------
 allCells_integrated <- readRDS(file=paste0(OUT_DIR,"HSC_final_UNIQUE_IDS.rds"))
 
 MPP_average_expression <- data.frame(AverageExpression(allCells_integrated, assays='SCT',features = c("THY1", "PTPRC", "ITGA6"), group.by="seurat_clusters")[[1]])
@@ -122,7 +114,7 @@ MPP_scaled_data$average <- rowMeans(MPP_scaled_data)
 row.names(MPP_scaled_data) <- str_replace(row.names(MPP_scaled_data),"X","")
 
 
-### plot linegraph of average MPP score per subcluster 
+# plot linegraph of average MPP score per subcluster ----------------------------------------- 
 MPP_scaled_data_ordered <- MPP_scaled_data[order(MPP_scaled_data$average, decreasing = TRUE),]
 
 df <- data.frame(
@@ -140,8 +132,8 @@ ggplot(df, aes(x=cluster, y=MPP_score)) + geom_point(shape=21,size=4.5, color="g
   theme(legend.position = "none") + labs(x="", y="CD90/CD49f/CD45RA Score")
 dev.off()
 
-# GGplot version of the UMAP ----------------------------------------------
 
+# GGplot version of the UMAP ----------------------------------------------
 embed_query_BM_new <- data.frame(Embeddings(allCells_integrated, reduction = "umap"))
 #check that cells are in the same order
 which(row.names(embed_query_BM_new) != row.names(allCells_integrated@meta.data) )
@@ -156,7 +148,6 @@ tiff(paste0(OUT_DIR, "subclusters_newUMAP_ggplot.tiff"), units="in", width=3.5, 
 ggplot(embed_query_BM_new, aes(x=UMAP_1, y=UMAP_2, color=cluster)) + geom_point(aes(color=cluster), alpha=0.6) + theme_classic() + theme(legend.position="none")+
   scale_color_manual(name= "", values=myColorScale) + labs(title="HSCs sub-clustered")
 dev.off()
-
 
 
 
@@ -175,7 +166,6 @@ for(i in 1:length(row.names(MPP_scaled_data)))
 df$MPP_avg_score <- MPP_average_score
 
 embed_query_BM$avg_score <- as.numeric(df$MPP_avg_score)
-
 
 tiff(paste0(OUT_DIR, "MPP_score_newUMAP_ggplot.tiff"), units="in", width=3.7, height=3, res=300)
 ggplot(embed_query_BM, aes(x=UMAP_1, y=UMAP_2, color=avg_score)) + geom_point(aes(color=avg_score), alpha=0.6) + theme_classic() + 
@@ -198,7 +188,8 @@ all<- subset(
   subset = UNIQUE_ID %in% unique_ids
 )
 
-##subset HSCs only
+
+# subset HSCs only ----------------------------------------------
 
 all_meta <- all@meta.data
 
@@ -210,10 +201,10 @@ HSC_full_obj <- subset(
   x = all,
   subset = temp %in% HSC_barcodes
 )
-
 HSC_full_obj$temp <- NULL
 
-#Add subcluster labels to meta data
+
+# Add subcluster labels to meta data -------------------------------------
 subcluster <- vector(length=length(row.names(HSC_full_obj@meta.data)))
 
 unique(allCells_integrated$seurat_clusters) #0-9
