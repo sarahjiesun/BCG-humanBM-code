3e305a305242e362348e727d650fd07a1322f1f7
Sarah Sun <42935401+sarahjiesun@users.noreply.github.com>
2024-07-02 10:04:15 -0500

    Add files via upload

---
+++ scRNA_analyses/2_main_analyses/cellRank/python scripts/S12_Tm3_cellrank.py
@@ -0,0 +1,85 @@
+import anndata
+import scvelo as scv
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+import os
+os.chdir('/project2/lbarreiro/users/Sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/Rprojects/projects_version2_Rv4.1/Analysis12_label_transfer_emmreml_edited/cellRank/wVelocity_input')
+
+adata = anndata.read_loom("/project2/lbarreiro/users/Sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/capture12_output/velocyto/capture12_output.loom")
+sample_obs = pd.read_csv("cellID_obs_S12_Tm3.csv")
+umap_cord = pd.read_csv("cell_embeddings_S12_Tm3.csv")
+cell_clusters = pd.read_csv("clusters_S12_Tm3.csv")
+
+adata = adata[np.isin(adata.obs.index,sample_obs["x"])]
+
+adata_index = pd.DataFrame(adata.obs.index)
+adata_index = adata_index.rename(columns = {'CellID':'Cell ID'})
+umap_cord = umap_cord.rename(columns = {'Unnamed: 0':'Cell ID'})
+umap_ordered = adata_index.merge(umap_cord, on = "Cell ID")
+umap_ordered = umap_ordered.iloc[:,1:]
+adata.obsm['X_umap'] = umap_ordered.values
+
+cell_clusters = cell_clusters.rename(columns = {'Unnamed: 0':'Cell ID'})
+clusters_ordered = adata_index.merge(cell_clusters, on = "Cell ID")
+clusters_ordered = clusters_ordered.iloc[:,1:]
+
+adata.uns['Cluster_colors'] = clusters_ordered.values
+adata.obs['Cluster_colors'] = clusters_ordered.values
+
+import sys
+import scvelo as scv
+import scanpy as sc
+import cellrank as cr
+import numpy as np
+os.chdir('/project2/lbarreiro/users/Sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/Rprojects/projects_version2_Rv4.1/Analysis12_label_transfer_emmreml_edited/cellRank/velocity_plots')
+
+scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+sc.tl.pca(adata)
+sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
+scv.pp.moments(adata, n_pcs=None, n_neighbors=None)
+scv.tl.recover_dynamics(adata, n_jobs=8)
+scv.tl.velocity(adata, mode="dynamical")
+scv.tl.velocity_graph(adata)
+
+scv.pl.velocity_embedding_stream(adata, basis="umap", legend_fontsize=12, title="S12_Tm3 Velocity Plot", smooth=0.8, min_mass=4, color="Cluster_colors", save="S12_Tm3_velocity_plot.png")
+
+adata.obsm['velocity_umap']
+vu = pd.DataFrame(adata.obsm['velocity_umap'])
+vu['Cell ID'] = adata_index
+vu = vu.rename(columns = {0:'coord1', 1:'coord2'})
+vu.to_csv('S12_Tm3_velocity_umap_coords.csv')
+
+
+os.chdir('/project2/lbarreiro/users/Sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/Rprojects/projects_version2_Rv4.1/Analysis12_label_transfer_emmreml_edited/cellRank/terminal_state_probs')
+
+
+from cellrank.tl.kernels import VelocityKernel
+vk = VelocityKernel(adata)
+print(vk)
+
+vk.compute_transition_matrix()
+from cellrank.tl.kernels import ConnectivityKernel
+ck = ConnectivityKernel(adata).compute_transition_matrix()
+combined_kernel = 0.8 * vk + 0.2 * ck
+from cellrank.tl.estimators import GPCCA
+
+g = GPCCA(combined_kernel)
+print(g)
+
+g.compute_schur(n_components=20)
+
+g.set_terminal_states({"MLP_a": adata[adata.obs["Cluster_colors"] == "MLP_a"].obs_names, "GMP_b": adata[adata.obs["Cluster_colors"]=="GMP_b"].obs_names, 
+"MLP_b": adata[adata.obs["Cluster_colors"] == "MLP_b"].obs_names, "GMP_a": adata[adata.obs["Cluster_colors"] == "GMP_a"].obs_names, "PreBNK": adata[adata.obs["Cluster_colors"] == "PreBNK"].obs_names,
+"MEP_a": adata[adata.obs["Cluster_colors"] == "MEP_a"].obs_names, "MEP_b": adata[adata.obs["Cluster_colors"] == "MEP_b"].obs_names,
+"MEP_c": adata[adata.obs["Cluster_colors"] == "MEP_c"].obs_names, "CMP_a": adata[adata.obs["Cluster_colors"] == "CMP_a"].obs_names, "CMP_b": adata[adata.obs["Cluster_colors"] == "CMP_b"].obs_names,
+"CMP_c": adata[adata.obs["Cluster_colors"] == "CMP_c"].obs_names})
+g.plot_terminal_states(discrete="TRUE", title="S12_Tm3 Terminal states", save="S12_Tm3_terminal_states.png")
+
+g.compute_absorption_probabilities(use_petsc=True, n_jobs=5, solver='gmres')
+adata.obsm["to_terminal_states"]
+trans_probs = pd.DataFrame(adata.obsm["to_terminal_states"])
+trans_probs = trans_probs.rename(columns = {0:'CMP_a', 1:'CMP_b', 2:'CMP_c', 3:'GMP_a', 4:'GMP_b', 5:'MEP_a', 6:'MEP_b', 7:'MEP_c', 8:'MLP_a', 9:'MLP_b', 10:'PreBNK'})  #rename all columns like this manually according to cluster
+trans_probs['Cell ID'] = adata_index
+trans_probs.to_csv('S12_Tm3_to_terminal_states.csv')
---
+++ scRNA_analyses/2_main_analyses/cellRank/workflow_main.py
@@ -0,0 +1,98 @@
+
+#First run velocyto (scripts in the velocyto folder) to generate loom files for each capture
+#Then run the Rscript in the cellrank folder to generate inputs for cellrank
+#run module load python -> conda activate cellrank -> python
+
+
+
+#CELL RANK estimators and kernels (low-level mode)
+
+
+import scvelo as scv
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import anndata
+
+import os
+os.chdir("")
+
+adata = anndata.read_loom("/project2/lbarreiro/users/Sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/capture2_output/velocyto/capture2_output.loom")
+sample_obs = pd.read_csv("cellID_obs.csv")
+umap_cord = pd.read_csv("cell_embeddings.csv")
+cell_clusters = pd.read_csv("clusters.csv")
+
+adata = adata[np.isin(adata.obs.index,sample_obs["x"])]
+
+adata_index = pd.DataFrame(adata.obs.index)
+adata_index = adata_index.rename(columns = {'CellID':'Cell ID'})
+umap_cord = umap_cord.rename(columns = {'Unnamed: 0':'Cell ID'})
+umap_ordered = adata_index.merge(umap_cord, on = "Cell ID")
+umap_ordered = umap_ordered.iloc[:,1:]
+adata.obsm['X_umap'] = umap_ordered.values
+
+cell_clusters = cell_clusters.rename(columns = {'Unnamed: 0':'Cell ID'})
+clusters_ordered = adata_index.merge(cell_clusters, on = "Cell ID")
+clusters_ordered = clusters_ordered.iloc[:,1:]
+
+adata.uns['Cluster_colors'] = clusters_ordered.values
+adata.obs['Cluster_colors'] = clusters_ordered.values
+
+import sys
+import scvelo as scv
+import scanpy as sc
+import cellrank as cr
+import numpy as np
+
+scv.pp.filter_and_normalize(adata, min_shared_counts=20, n_top_genes=2000)
+sc.tl.pca(adata)
+sc.pp.neighbors(adata, n_pcs=30, n_neighbors=30)
+scv.pp.moments(adata, n_pcs=None, n_neighbors=None)
+scv.tl.recover_dynamics(adata, n_jobs=8)
+scv.tl.velocity(adata, mode="dynamical")
+scv.tl.velocity_graph(adata)
+
+scv.pl.velocity_embedding_stream(adata, basis="umap", legend_fontsize=12, title="", smooth=0.8, min_mass=4, color="Cluster_colors", save="test_velocity_plot.png")
+
+
+from cellrank.tl.kernels import VelocityKernel
+vk = VelocityKernel(adata)
+print(vk)
+
+vk.compute_transition_matrix()
+from cellrank.tl.kernels import ConnectivityKernel
+ck = ConnectivityKernel(adata).compute_transition_matrix()
+combined_kernel = 0.8 * vk + 0.2 * ck
+from cellrank.tl.estimators import GPCCA
+
+g = GPCCA(combined_kernel)
+print(g)
+
+g.compute_schur(n_components=20)
+#g.plot_spectrum()
+g.compute_macrostates(n_states=11, cluster_key="Cluster_colors")  #use 11 but ok if it defaults to 12
+g.plot_macrostates(same_plot=False, save="test_macrostates_separate.png")
+g.plot_macrostates(discrete=True, save="test_macrostates_discrete.png")
+
+g.set_terminal_states_from_macrostates(names=["MEP_b", "CMP_c", "CMP_a", "CMP_b_1", "CMP_b_2", "GMP_b", "PreBNK"]) #combine macrostates by original clusters
+g.plot_terminal_states()
+
+
+g.compute_absorption_probabilities(use_petsc=True, n_jobs=5, solver='gmres')
+#g.plot_absorption_probabilities(adata, same_plot=False)  #THIS PART NEEDS MORE THAN 50GB MEM - GIVES AN ERROR
+
+
+##TO SAVE RESULTS
+
+#example of how data frames can be saved
+adata.obsm["to_terminal_states"]
+trans_probs = pd.DataFrame(adata.obsm["to_terminal_states"])
+trans_probs = trans_probs.rename(columns = {0:'MEP_b', 1:'CMPc'})  #rename all columns like this manually according to cluster
+trans_probs['Cell ID'] = adata_index
+trans_probs.to_csv('test_to_terminal_states.csv')
+
+adata.obsm["terminal_states_memberships"]
+memb = pd.DataFrame(adata.obsm["terminal_states_memberships"])
+memb = memb.rename(columns = {0:'MEP_b', 1:'CMPc'})  #rename all columns like this manually according to cluster
+memb['Cell ID'] = adata_index
+memb.to_csv('test_terminal_states_memberships.csv')
