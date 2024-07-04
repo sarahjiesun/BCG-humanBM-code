3037cd10a49822c0925eb2275da9b7b027fd56a4
Sarah Sun <42935401+sarahjiesun@users.noreply.github.com>
2024-07-04 13:08:16 -0500

    Update 4_get_diff_regulons_GMP_a.R

--- scRNA_analyses/2_main_analyses/pyscenic/4_get_diff_regulons_GMP_a.R
+++ scRNA_analyses/2_main_analyses/pyscenic/4_get_diff_regulons_GMP_a.R
@@ -16,7 +16,7 @@ library(SCopeLoomR)
 library(doRNG)
 library(ggrepel)
 
-setwd("/project2/lbarreiro/users/Sarah/HUMAN_BM_PROJECT/BM_CD34_scRNA/Rprojects/projects_version2_Rv4.1/Analysis12_label_transfer_emmreml_edited/pyscenic")
+setwd("/scRNA_analyses/2_main_analyses/pyscenic")
 OUT_DIR <- "get_diff_regulons_outs/"
 dir.create(OUT_DIR)
 
@@ -116,9 +116,7 @@ write.csv(logfc_mat, file=paste0(OUT_DIR, "GMP_a_logfc_mat_no_outliers.csv"))
 
 pval <- vector(length=length(row.names(logfc_mat)))
 pval_wilcox <- vector(length=length(row.names(logfc_mat)))
-plot_list <- list()
 tf_name <- vector()
-plot_count <- 1
 for(i in 1:length(row.names(logfc_mat)))
 {
   values <- logfc_mat[i, ]
@@ -138,138 +136,6 @@ padj_wilcox <- p.adjust(pval_wilcox, method='BH')
 wilcox_data <- data.frame(cbind(pval_wilcox, padj_wilcox))
 row.names(wilcox_data) <- row.names(logfc_mat)
 write.csv(wilcox_data, file=paste0(OUT_DIR, "GMP_a_wilcox_data_no_outliers.csv")) ##RENAME GMP c1
-
-
-
-
-#plots for specific regulons 
-
-regulons <- c("EGR1","EGR3", "ERG", "KLF5", "KLF6", "KLF10", "ELF2", "ELK3", "ERF", "ETV6", "KLF3")
-colors_list <- c("darkgrey", "darkgrey", "darkgrey", "darkgrey", "darkgrey", "darkgrey", "darkgrey", "#E15759", "darkgrey", "darkgrey", "darkgrey")
-plot_list <- list()
-
-for(i in 1:length(regulons))
-{
-  if(regulons[i] %in% row.names(logfc_mat))
-  {
-    values <- logfc_mat[regulons[i], ]
-    BCG_donors <- unique(meta_data$donor[which(meta_data$vaccination == "BCG")])
-    CTL_donors <- unique(meta_data$donor[which(meta_data$vaccination == "CTL")])
-    CTL_vals <- values[which(names(values) %in% CTL_donors)]
-    BCG_vals <- values[which(names(values) %in% BCG_donors)]
-    
-    res <- wilcox.test(BCG_vals, CTL_vals)
-    pval <- res$p.value
-    
-    donors <- c(names(values)[which(names(values) %in% BCG_donors)], names(values)[which(names(values) %in% CTL_donors)])
-    lab <- rep("no",length(donors))
-    lab[which(!(donors %in% c("S8", "S10", "S12", "S14", "S16")))] <- "yes"
-    
-    df_plot <- data.frame(
-      vals <- c(BCG_vals, CTL_vals),
-      group <- c(rep("BCG", length(BCG_vals)), rep("CTL", length(CTL_vals))),
-      d <- donors,
-      l <- lab
-    )
-    colnames(df_plot) <- c("vals", "group", "d", "l")
-    
-    #Make plot where values are shifter relative to controls
-    subset <- subset(df_plot, df_plot$group == "CTL")
-    subset_median <- median(subset$vals)
-    
-    new_vals <- df_plot$vals - subset_median
-    df_plot_new <- data.frame(
-      val_corrected <- new_vals,
-      group <- df_plot$group,
-      d <- df_plot$d,
-      l <- df_plot$l
-    )
-    colnames(df_plot_new) <- c("vals", "group", "d", "l")
-    
-    p <- ggplot(data=df_plot_new, aes(x=group, y=vals, fill=group, label=d)) + geom_boxplot(aes(fill=group), alpha=0.8)  + geom_jitter(shape=21, size=2, stroke=1, alpha=1, position=position_jitter(0.2), aes(fill=group))+ theme_classic()+
-      labs(x="", y="Normalized TF Activity change", fill="Group", title=paste0(regulons[i]," in GMPa\npval = ", round(pval, digits=2))) + scale_fill_manual(values=c(colors_list[i],"lightgrey")) +
-      theme(plot.title=element_text(size=10)) +  theme(legend.position = "none") + theme(axis.title=element_text (size=9))
-    
-    plot_list[[i]] <- p
-  }
-  
-}
-
-
-for(k in 1:length(plot_list)) 
-{
-  file_name = paste0(OUT_DIR, regulons[k], "_regulon_GMP_a.tiff")
-  tiff(file_name, units="in", width=2, height=2.5, res=300)
-  print(plot_list[[k]])
-  dev.off()
-  print(k)
-  
-}
-
-
-
-
-#plots for specific regulons broad TF group names 
-
-regulons <- c( "KLF3", "KLF10", "ELK3")
-broad_names <- c( "KLF2/3/6", "KLF/SP_2", "ETS_3")
-colors_list <- c("darkgrey", "darkgrey",  "#E15759")
-plot_list <- list()
-
-for(i in 1:length(regulons))
-{
-  values <- logfc_mat[regulons[i], ]
-  BCG_donors <- unique(meta_data$donor[which(meta_data$vaccination == "BCG")])
-  CTL_donors <- unique(meta_data$donor[which(meta_data$vaccination == "CTL")])
-  CTL_vals <- values[which(names(values) %in% CTL_donors)]
-  BCG_vals <- values[which(names(values) %in% BCG_donors)]
-  
-  res <- wilcox.test(BCG_vals, CTL_vals)
-  pval <- res$p.value
-  
-  donors <- c(names(values)[which(names(values) %in% BCG_donors)], names(values)[which(names(values) %in% CTL_donors)])
-  lab <- rep("no",length(donors))
-  lab[which(!(donors %in% c("S8", "S10", "S12", "S14", "S16")))] <- "yes"
-  
-  df_plot <- data.frame(
-    vals <- c(BCG_vals, CTL_vals),
-    group <- c(rep("BCG", length(BCG_vals)), rep("CTL", length(CTL_vals))),
-    d <- donors,
-    l <- lab
-  )
-  colnames(df_plot) <- c("vals", "group", "d", "l")
-  
-  #Make plot where values are shifter relative to controls
-  subset <- subset(df_plot, df_plot$group == "CTL")
-  subset_median <- median(subset$vals)
-  
-  new_vals <- df_plot$vals - subset_median
-  df_plot_new <- data.frame(
-    val_corrected <- new_vals,
-    group <- df_plot$group,
-    d <- df_plot$d,
-    l <- df_plot$l
-  )
-  colnames(df_plot_new) <- c("vals", "group", "d", "l")
-  
-  p <- ggplot(data=df_plot_new, aes(x=group, y=vals, fill=group, label=d)) + geom_boxplot(aes(fill=group), alpha=0.8)  + geom_jitter(shape=21, size=2, stroke=1, alpha=1, position=position_jitter(0.2), aes(fill=group))+ theme_classic()+
-    labs(x="", y="Normalized TF Activity change", fill="Group", title=paste0(broad_names[i]," in GMP\npval = ", round(pval, digits=2))) + scale_fill_manual(values=c(colors_list[i],"lightgrey")) +
-    theme(plot.title=element_text(size=12)) +  theme(legend.position = "none") + theme(axis.title=element_text (size=10))
-  
-  plot_list[[i]] <- p
-}
-
-
-for(k in 1:length(plot_list)) 
-{
-  file_name = paste0(OUT_DIR, regulons[k], "_broad_name_GMP_a_excluded.tiff")
-  tiff(file_name, units="in", width=2, height=2.5, res=300)
-  print(plot_list[[k]])
-  dev.off()
-  print(k)
-  
-}
-
 
 
 
